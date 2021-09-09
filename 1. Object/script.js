1e9466a3644b120d0d8c01f7b1d6e43fff67ddf0
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-09 09:02:52 +0700

    1.3 Prototype

--- 1. Object/script.js
+++ 1. Object/script.js
@@ -59,36 +59,65 @@
 // console.log(mhs1);
 // console.log(mhs2);
 
-// Object.create()
-const mahasiswaMethod = {
-  makan: function (porsi) {
-    this.energi += porsi;
-  },
-};
+// // Object.create()
+// const mahasiswaMethod = {
+//   makan: function (porsi) {
+//     this.energi += porsi;
+//   },
+// };
 
+// function Mahasiswa(nama, energi) {
+//   let mahasiswa = Object.create(mahasiswaMethod); // membawa object mahasiswaMethod
+//   // mirip seperti inheritance
+
+//   mahasiswa.nama = nama;
+//   mahasiswa.energi = energi;
+
+//   return mahasiswa;
+// }
+
+// let mhs1 = Mahasiswa("Fahrul", 10);
+// let mhs2 = Mahasiswa("Ujang", 15);
+// console.log(mhs1);
+// console.log(mhs2);
+
+// // contoh 2
+// let obj1 = {
+//   nama: "Fahrul",
+//   umur: 19,
+// };
+
+// let obj2 = Object.create(obj1);
+// obj2.nama = "Ujang"; // meng-override yang atas
+// obj2.alamat = "Bandung";
+// console.log(obj2.nama);
+
+// // Implementasi Prototype
 function Mahasiswa(nama, energi) {
-  let mahasiswa = Object.create(mahasiswaMethod); // membawa object mahasiswaMethod
-  // mirip seperti inheritance
-
-  mahasiswa.nama = nama;
-  mahasiswa.energi = energi;
-
-  return mahasiswa;
+  this.nama = nama;
+  this.energi = energi;
 }
 
-let mhs1 = Mahasiswa("Fahrul", 10);
-let mhs2 = Mahasiswa("Ujang", 15);
-console.log(mhs1);
-console.log(mhs2);
-
-// contoh 2
-let obj1 = {
-  nama: "Fahrul",
-  umur: 19,
+Mahasiswa.prototype.makan = function (porsi) {
+  this.energi += porsi;
 };
 
-let obj2 = Object.create(obj1);
-obj2.nama = "Ujang"; // meng-override yang atas
-obj2.alamat = "Bandung";
+Mahasiswa.prototype.main = function (jam) {
+  this.energi -= jam;
+};
 
-console.log(obj2.nama);
+let mhs1 = new Mahasiswa("Mhs1", 10);
+
+// Konsep Class
+class Person {
+  constructor(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+  }
+
+  makan(porsi) {
+    this.energi += porsi;
+  }
+}
+
+let person1 = new Person("Fahrul", 19);
