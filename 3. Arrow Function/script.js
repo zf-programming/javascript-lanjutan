6522b0515c84edced941f1479826204c78f80f2d
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-11 13:07:52 +0700

    3.2 Arrow Function Lanjutan (this)

--- 3. Arrow Function/script.js
+++ 3. Arrow Function/script.js
@@ -51,3 +51,56 @@
 //   jumlah: value.length,
 // }));
 // console.table(jumlahHurufObj);
+
+//// 3.2 Arrow Function Lanjutan (this)
+// Arrow Function tidak memiliki konsep this
+// this pada Arrow merujuk pada this punya Parent
+// kalau tdk ada maka akan this = window
+
+// // Object dengan Constructor Function
+// const Mahasiswa = function () {
+//   this.nama = "Fahrul";
+//   this.umur = 19;
+
+//   this.sayName = function () {
+//     return `Nama ${this.nama}`;
+//   };
+
+//   // arrow function
+//   this.sayAge = () => `Umur ${this.umur}`;
+// };
+
+// const fahrul = new Mahasiswa();
+// console.log(fahrul.sayName());
+// console.log(fahrul.sayAge());
+
+// // // Object Literal
+// const mhs1 = {
+//   nama: "Yanto",
+//   umur: 22,
+//   sayName: function () {
+//     return `Nama ${this.nama}`;
+//   },
+
+//   //arrow function
+//   sayAge: () => `Umur ${this.umur}`, // undefined (karena di window tidak ada umur)
+// };
+
+// console.log(mhs1.sayName());
+// console.log(mhs1.sayAge());
+
+// // Contoh Penerapan
+// // class
+// class Person {
+//   constructor(nama, umur) {
+//     this.nama = nama;
+//     this.umur = umur;
+
+//     // this pada setInverval mengacu ke windows
+//     // bisa di akali dengan arrow function agar this mengacu ke parent
+//     setInterval(() => console.log(this.umur++), 1000);
+//   }
+// }
+
+// const person1 = new Person("person1", 20);
+// console.log(person1);
