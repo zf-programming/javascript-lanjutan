f4d46e3eff5af3f688b19ef456ff71333acaf1ec
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-17 15:05:37 +0700

    9. Konsep Asynchronous

---
+++ 9. Asynchronous/script.js
@@ -0,0 +1,19 @@
+// /// 9. Asynchronous
+
+// contoh konsep synchronous
+function sayHello() {
+  return `Hello!`;
+}
+// berurutan
+console.log('Synchronous');
+console.log('mulai');
+console.log(sayHello());
+console.log('selesai');
+
+// contoh asynchronous
+console.log('Asynchronous');
+console.log('mulai');
+setTimeout(() => console.log('Hello 2'), 3000); // akan dilewati dan muncul setelah 3 detik
+console.log('selesai');
+
+// cek di http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
